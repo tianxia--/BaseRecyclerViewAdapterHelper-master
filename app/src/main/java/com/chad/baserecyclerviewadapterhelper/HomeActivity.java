b32e88259372c1488df2416fe2c40f1fbeb1f01f
技术产品部-技术1部-客户端-陈鹏飞 <chenpengfei_d@DUOKU.COM>
2016-04-26 11:14:41 +0800

    Initial commit

---
+++ app/src/main/java/com/chad/baserecyclerviewadapterhelper/HomeActivity.java
@@ -0,0 +1,54 @@
+package com.chad.baserecyclerviewadapterhelper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.chad.baserecyclerviewadapterhelper.adapter.HomeAdapter;
+import com.chad.baserecyclerviewadapterhelper.entity.HomeItem;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public class HomeActivity extends Activity {
+    private static final Class<?>[] ACTIVITY = {AnimationUseActivity.class, MultipleItemUseActivity.class, HeaderAndFooterUseActivity.class, PullToRefreshUseActivity.class, SectionUerActivity.class};
+    private static final String[] TITLE = {"Animation Use", "MultipleItem Use", "HeaderAndFooter Use", "PullToRefresh Use", "Section Use"};
+    private ArrayList<HomeItem> mDataList;
+    private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        initData();
+        BaseQuickAdapter homeAdapter = new HomeAdapter(this, R.layout.home_item_view, mDataList);
+        homeAdapter.setOnRecyclerViewItemClickListener(new BaseQuickAdapter.OnRecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent intent = new Intent(HomeActivity.this, ACTIVITY[position]);
+                startActivity(intent);
+            }
+        });
+        mRecyclerView.setAdapter(homeAdapter);
+
+    }
+
+    private void initData() {
+        mDataList = new ArrayList<>();
+        for (int i = 0; i < TITLE.length; i++) {
+            HomeItem item = new HomeItem();
+            item.setTitle(TITLE[i]);
+            item.setActivity(ACTIVITY[i]);
+            mDataList.add(item);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/chad/baserecyclerviewadapterhelper/adapter/QuickAdapter.java
@@ -0,0 +1,32 @@
+package com.chad.baserecyclerviewadapterhelper.adapter;
+
+import android.content.Context;
+
+import com.chad.baserecyclerviewadapterhelper.R;
+import com.chad.baserecyclerviewadapterhelper.data.DataServer;
+import com.chad.baserecyclerviewadapterhelper.entity.Status;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public class QuickAdapter extends BaseQuickAdapter<Status> {
+    public QuickAdapter(Context context) {
+        super(context, R.layout.tweet, DataServer.getSampleData(100));
+    }
+
+    public QuickAdapter(Context context, int dataSize) {
+        super(context, R.layout.tweet, DataServer.getSampleData(dataSize));
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Status item) {
+        helper.setText(R.id.tweetName, item.getUserName())
+                .setText(R.id.tweetText, item.getText())
+                .setText(R.id.tweetDate, item.getCreatedAt())
+                .setImageUrl(R.id.tweetAvatar, item.getUserAvatar())
+                .setVisible(R.id.tweetRT, item.isRetweet())
+                .linkify(R.id.tweetText);
+    }
+}
